7fbd397b3b994827f71189324e7020595f91ab62
pavanpatchikarla <76902617+P-1000@users.noreply.github.com>
2023-10-16 00:42:00 +0530

    test

--- index.js
+++ index.js
@@ -1,63 +1,67 @@
 #!/usr/bin/env node
 import chalk from "chalk";
 import figlet from "figlet";
-import readline from "readline";
 import { execSync } from "child_process";
-import OpenAI from "openai";
+import inquirer from "inquirer";
+import autocomplete from "inquirer-autocomplete-prompt";
 
+inquirer.registerPrompt("autocomplete", autocomplete);
 
-
-
-const openai = new OpenAI({
-  apiKey: "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"
-});
-
-async function runCompletion () {
-  const completion = await openai.completions.create({
-    model: "text-davinci-003",
-    prompt: "How are you today?",
-  });
-  console.log(completion.choices[0].text);
+const displayGitStatus = () => {
+  try {
+    const statusOutput = execSync("git status").toString();
+    console.log(chalk.bgCyan.white.bold("Git Status:"));
+    console.log(statusOutput);
+  } catch (error) {
+    console.error(`${chalk.bgRed.white("Error executing git status:")} ${error.message}`);
   }
+};
 
-const main = () => {
-  // runCompletion();
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  console.log(chalk.green(figlet.textSync("Jutsu-Git", { horizontalLayout: "full" })));
+const main = async () => {
+  // console.log(chalk.green(figlet.textSync("Jutsu-Git", { horizontalLayout: "full" }));
 
   try {
-    // Use Git to list all branches
+    displayGitStatus();
+
     const branches = execSync("git branch --list")
       .toString()
       .split("\n")
       .map((branch) => branch.trim());
 
-    console.log(chalk.bgBlue.white.bold("Available Branches:"));
-    branches.forEach((branch) => console.log(`- ${branch}`));
+    const branchPrompt = inquirer.prompt([
+      {
+        type: "autocomplete",
+        name: "branch",
+        message: "Select a branch to commit to:",
+        source: (answersSoFar, input) => {
+          input = input || "";
+          return branches.filter((branch) => branch.includes(input));
+        },
+        result(name) {
+          return this.focused ? chalk.cyan(name) : name;
+        },
+      },
+      {
+        type: "input",
+        name: "commitMessage",
+        message: "Enter commit message (or press Enter for default):",
+      },
+    ]);
 
-    rl.question("Enter commit message (or press Enter for default): ", (commitMessage) => {
-      rl.question("Enter the branch name you want to commit to (or press Enter for default): ", (branchName) => {
-        rl.close();
+    const { branch, commitMessage } = await branchPrompt;
+    const defaultBranchName = "main";
+    const finalBranchName = branch || defaultBranchName;
+    const defaultCommitMessage = "Default commit message";
+    const finalCommitMessage = commitMessage.trim() || defaultCommitMessage;
 
-        const defaultCommitMessage = "Default commit message";
-        const defaultBranchName = "main";
-        const finalCommitMessage = commitMessage.trim() || defaultCommitMessage;
-        const finalBranchName = branchName.trim() || defaultBranchName;
+    console.log(chalk.yellow("Committing changes..."));
 
-        console.log(chalk.yellow("Committing changes..."));
-
-        try {
-          execSync(`git add . && git commit -m "${finalCommitMessage}" && git push origin ${finalBranchName}`);
-          console.log(chalk.bgGreen.white.bold("Git added, committed, and pushed successfully!"));
-        } catch (error) {
-          console.error(`${chalk.bgRed.white.bold("Error executing git commands:")} ${error.message}`);
-        }
-      });
-    });
+    try {
+      execSync(`git add . && git commit -m "${finalCommitMessage}" && git push origin ${finalBranchName}`);
+      console.log(chalk.bgGreen.white.bold("Git added, committed, and pushed successfully!"));
+    } catch (error) {
+      console.error(`${chalk.bgRed.white.bold("Error executing git commands:")} ${error.message}`);
+    }
   } catch (error) {
     console.error(`${chalk.bgRed.white("Error executing git commands:")} ${error.message}`);
   }
