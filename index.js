6ea298ae70198b07513ea2f90ece899601130283
pavanpatchikarla <76902617+P-1000@users.noreply.github.com>
2023-12-31 00:21:01 +0530

    new changes

--- index.js
+++ index.js
@@ -1,52 +1,54 @@
 #!/usr/bin/env node
-import chalk from "chalk";
-import figlet from "figlet";
-import { execSync } from "child_process";
-import inquirer from "inquirer";
-import autocomplete from "inquirer-autocomplete-prompt";
+import chalk from 'chalk';
+import figlet from 'figlet';
+import { execSync } from 'child_process';
+import inquirer from 'inquirer';
+import autocomplete from 'inquirer-autocomplete-prompt';
 
-inquirer.registerPrompt("autocomplete", autocomplete);
+inquirer.registerPrompt('autocomplete', autocomplete);
 
-
-const displayModifiedFiles = () => {
+const executeCommand = (command) => {
   try {
-    const statusOutput = execSync("git status --short").toString();
-    const modifiedFiles = statusOutput
-      .split('\n')
-      .filter(line => line.startsWith(' M '))
-      .map(line => line.replace(' M ', ''))
-      .join('\n');
-
-    if (modifiedFiles) {
-      console.log(chalk.bgCyan.white.bold("Modified Files:"));
-      console.log(modifiedFiles);
-    } else {
-      console.log(chalk.bgGreen.white.bold("No modified files."));
-    }
+    const result = execSync(command, { stdio: 'pipe', encoding: 'utf-8' });
+    return result.trim();
   } catch (error) {
-    console.error(`${chalk.bgRed.white.bold("Error executing git status:")} ${error.message}`);
+    throw new Error(`Error executing command "${command}": ${error.message}`);
   }
 };
 
-const main = async () => {
-
-  console.log(chalk.green(figlet.textSync("Jutsu-Git" , {horizontalLayout: "full"})));
-
+const displayModifiedFiles = () => {
   try {
-    displayModifiedFiles();
+    const statusOutput = executeCommand('git status --short');
+    const modifiedFiles = statusOutput
+      .split('\n')
+      .filter((line) => line.startsWith(' M '))
+      .map((line) => line.replace(' M ', ''))
+      .join('\n');
 
-    const branches = execSync("git branch --list")
-      .toString()
-      .split("\n")
+    if (modifiedFiles) {
+      console.log(chalk.bgCyan.white.bold('Modified Files:'));
+      console.log(modifiedFiles);
+    } else {
+      console.log(chalk.bgGreen.white.bold('No modified files.'));
+    }
+  } catch (error) {
+    console.error(`${chalk.bgRed.white.bold('Error executing git status:')} ${error.message}`);
+  }
+};
+
+const promptForBranchAndCommit = async () => {
+  try {
+    const branches = executeCommand('git branch --list')
+      .split('\n')
       .map((branch) => branch.trim());
 
     const branchPrompt = inquirer.prompt([
       {
-        type: "autocomplete",
-        name: "branch",
-        message: "Select a branch to commit to:",
+        type: 'autocomplete',
+        name: 'branch',
+        message: 'Select a branch to commit to:',
         source: (answersSoFar, input) => {
-          input = input || "";
+          input = input || '';
           return branches.filter((branch) => branch.includes(input));
         },
         result(name) {
@@ -54,30 +56,45 @@ const main = async () => {
         },
       },
       {
-        type: "input",
-        name: "commitMessage",
-        message: "Enter commit message (or press Enter for default):",
+        type: 'input',
+        name: 'commitMessage',
+        message: 'Enter commit message (or press Enter for default):',
       },
     ]);
 
-    const { branch, commitMessage } = await branchPrompt;
-    const defaultBranchName = "main";
+    return await branchPrompt;
+  } catch (error) {
+    throw new Error(`Error prompting for branch and commit: ${error.message}`);
+  }
+};
+
+const main = async () => {
+  console.log(chalk.green(figlet.textSync('Jutsu-Git', { horizontalLayout: 'full' })));
+
+  try {
+    displayModifiedFiles();
+
+    const { branch, commitMessage } = await promptForBranchAndCommit();
+
+    const defaultBranchName = 'main';
     const finalBranchName = branch || defaultBranchName;
-    const defaultCommitMessage = "Default commit message";
+    const defaultCommitMessage = 'Default commit message';
     const finalCommitMessage = commitMessage.trim() || defaultCommitMessage;
 
-    console.log(chalk.red("Branch: ") + chalk.cyan(finalBranchName));
+    console.log(chalk.red('Branch: ') + chalk.cyan(finalBranchName));
 
-    console.log(chalk.green(figlet.textSync("Wait Bro!" , {horizontalLayout: "full"})));
+    console.log(chalk.green(figlet.textSync('Wait Bro!', { horizontalLayout: 'full' })));
 
     try {
-      execSync(`git add . && git commit -m "${finalCommitMessage}" && git push origin ${finalBranchName}`);
-      console.log(chalk.bgGreen.white.bold("Git added, committed, and pushed successfully!"));
+      executeCommand(`git add .`);
+      executeCommand(`git commit -m "${finalCommitMessage}"`);
+      executeCommand(`git push origin ${finalBranchName}`);
+      console.log(chalk.bgGreen.white.bold('Git added, committed, and pushed successfully!'));
     } catch (error) {
-      console.error(`${chalk.bgRed.white.bold("Error executing git commands:")} ${error.message}`);
+      console.error(`${chalk.bgRed.white.bold('Error executing git commands:')} ${error.message}`);
     }
   } catch (error) {
-    console.error(`${chalk.bgRed.white("Error executing git commands:")} ${error.message}`);
+    console.error(`${chalk.bgRed.white('Error executing script:')} ${error.message}`);
   }
 };
 
