7ad21cadb861ece7b7d1c21a66f6110836a1b129
pavanpatchikarla <76902617+P-1000@users.noreply.github.com>
2024-02-10 18:29:35 +0530

    Jutsu-Git Commit

--- Gemini.js
+++ Gemini.js
@@ -1,12 +1,12 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import chalk from "chalk";
 
-// Access your API key (see "Set up your API key" above)
+
+
 const API_KEY = "AIzaSyDkVHUDN95kPp9-yADpuxhGnB4scKTDcyw";
 const genAI = new GoogleGenerativeAI(API_KEY);
 
 async function getCommitMessage(diffinput) {
-    
-  // For text-only input, use the gemini-pro model
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
   const prompt = `
@@ -40,7 +40,7 @@ async function getCommitMessage(diffinput) {
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const text = response.text();
-  console.log(text);
+  console.log(chalk.bgGreen.white.bold(`AI Generated Commit Message:\n${text}`));
 }
 
 export default getCommitMessage;
