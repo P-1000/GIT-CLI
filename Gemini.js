1bd8cba90636ad9e3b1b87dddf65cbb0f99ddd8f
pavanpatchikarla <76902617+P-1000@users.noreply.github.com>
2024-02-10 18:25:06 +0530

    Jutsu-Git Commit

--- Gemini.js
+++ Gemini.js
@@ -8,7 +8,7 @@ async function getCommitMessage(diffinput) {
   // For text-only input, use the gemini-pro model
   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
-  const prompt = "write a commit message for the following changes:\n" + diffinput ;
+  const prompt = "write a commit message for the following changes :\n" + diffinput + " make it short and include the main changes and some technical details and references if needed but make it short and clear." ;
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
